a7a9fb1bb81a8ef97acf05589324cfce80cc2c02
noordseoostende <voropayev1@mail.ru>
2021-02-03 22:09:03 +0100

    ik begon in react schrijven

--- common/models/post-image.js
+++ common/models/post-image.js
@@ -1,5 +1,32 @@
-'use strict';
+"use strict";
+const sharp = require("sharp");
+const fs = require("fs");
 
-module.exports = function(Postimage) {
+module.exports = function (PostImage) {
+  PostImage.upload = function (ctx, options, access_token, post_id, cb) {
+    if (!options) options = {};
 
+    ctx.req.params.container = "postImages";
+    if (!fs.existsSync("./server/storage/" + ctx.req.params.container)) {
+      fs.mkdirSync("./server/storage/" + ctx.req.params.container);
+    }
+
+    PostImage.app.models.ImageFile;
+  };
+
+  PostImage.remoteMethod("upload", {
+    description: "Uploads a file",
+    accepts: [
+      { arg: "ctx", type: "object", http: { source: "context" } },
+      { arg: "options", type: "object", http: { source: "query" } },
+      { arg: "access_token", type: "string" },
+      { arg: "post_id", type: "string" },
+    ],
+    returns: {
+      arg: "fileObject",
+      type: "object",
+      root: true,
+    },
+    http: { verb: "post" },
+  });
 };
